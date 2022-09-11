14324ef02a9d441bb49962018f8ebc7e4b956744
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-09-11 18:38:35 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//Andrew McGuan
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  //Declare variables 
+  double C , R ;
+
+  // output request
+  cout << "Input values for C (Outer most crust in cm) , R (Radius of the pizza in cm)" << endl;
+  cin >> C; cin >> R;
+  }
